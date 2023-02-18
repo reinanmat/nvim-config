40d662c732cd86213093ada025ce556d3f123b6f
reinanmat <reinan266@gmail.com>
2023-02-18 16:48:03 -0300

    add new remaps

--- lua/myLuaModule/remap.lua
+++ lua/myLuaModule/remap.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set("i", "jk", "<ESC>")
 
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", "m: '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", "m: '<-2<CR>gv=gv")
@@ -12,3 +12,4 @@ vim.keymap.set("n", "<leader>g", "gg=G")
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("n", "<leader>q", ":q<CR>")
 
+vim.g.floaterm_keymap_new    = '<leader>ft'
