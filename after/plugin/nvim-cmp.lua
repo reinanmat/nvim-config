e90af20812763f48724b709b7ecf322b97b50255
reinanmat <reinan266@gmail.com>
2023-03-02 19:13:50 -0300

    refactored

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -1,23 +1,20 @@
-
 local cmp_status, cmp = pcall(require, "cmp")
 if not cmp_status then
   return
 end
-
 
 local luasnip_status, luasnip = pcall(require, "luasnip")
 if not luasnip_status then
   return
 end
 
-
 local lspkind_status, lspkind = pcall(require, "lspkind")
 if not lspkind_status then
   return
 end
 
-
 require("luasnip/loaders/from_vscode").lazy_load()
+
 
 vim.opt.completeopt = "menu,menuone,noselect"
 
@@ -32,11 +29,11 @@ cmp.setup({
     ["<C-j>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(), 
-    ["<C-e>"] = cmp.mapping.abort(), 
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
   }),
-  
+
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
     { name = "luasnip" },
