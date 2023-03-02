dfed20ca9706dd2d3ff72ca30051b2efe25e1dcb
reinanmat <reinan266@gmail.com>
2023-03-02 19:08:07 -0300

    feat: removed floaterm and added lualine

--- after/plugin/floaterm.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set("n", "<leader>ft", ":FloatermNew --cwd=<buffer><CR>")
-
-vim.g.floaterm_gitcommit='floaterm'
-vim.g.floaterm_width=0.8
-vim.g.floaterm_height=0.8
-vim.g.floaterm_wintitle=0
-vim.g.floaterm_autoclose=1
--- lua/myLuaModule/packer.lua
+++ lua/myLuaModule/packer.lua
@@ -1,47 +1,52 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
 
-	use {
+	--	Plugins Mangagers
+	use	('williamboman/mason-lspconfig.nvim')
+	use	('williamboman/mason.nvim')
+	use	('wbthomason/packer.nvim')
+	--	Navigation
+	use	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
-	use ({
+	use	('ThePrimeagen/harpoon')
+	use	('prichrd/netrw.nvim')
+	use	('mbbill/undotree')
+	--	Syntax Highlighting
+	use	('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use	('Vimjas/vim-python-pep8-indent')
+	use	('nvim-treesitter/playground')
+	use	('tpope/vim-commentary')
+	--	Aesthetics
+	use	("m4xshen/smartcolumn.nvim")
+	use	({ 'nvim-lualine/lualine.nvim', opt = true })
+	use	{
 		'rose-pine/neovim',
 		as = 'rose-pine',
 		config = function()
 			require("rose-pine").setup()
 			vim.cmd('colorscheme rose-pine')
 		end
-	})
-	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use ('nvim-treesitter/playground')
-	use ('nvim-lua/plenary.nvim')
-	use ('ThePrimeagen/harpoon')
-	use ('mbbill/undotree')
+	}
+	--	42 Plugins
 	use	("42paris/42header")
-	use ('Vimjas/vim-python-pep8-indent')
-	use ('tpope/vim-commentary')
-	use ('saadparwaiz1/cmp_luasnip')
-	use ('hrsh7th/nvim-cmp')
-	use ('hrsh7th/cmp-buffer')
-	use ('hrsh7th/cmp-path')
-	use ('L3MON4D3/LuaSnip')
-	use ('rafamadriz/friendly-snippets')
-	use ('neovim/nvim-lspconfig')
-	use ('williamboman/mason.nvim')
-	use	('williamboman/mason-lspconfig.nvim')
-	use ('onsails/lspkind.nvim')
-	use ("hrsh7th/cmp-nvim-lsp")
-	use ({ "glepnir/lspsaga.nvim", branch = "main" })
-	use ("nvim-tree/nvim-web-devicons")
-	use ('prichrd/netrw.nvim')
+	--	Autocompletion
+	use	('hrsh7th/cmp-buffer')
+	use	('hrsh7th/nvim-cmp')
+	use	('hrsh7th/cmp-path')
+	--	Snippets
+	use	('rafamadriz/friendly-snippets')
+	use	('saadparwaiz1/cmp_luasnip')
+	use	('L3MON4D3/LuaSnip')
+	--	Lsp
+	use	({ "glepnir/lspsaga.nvim", branch = "main" })
+	use	('neovim/nvim-lspconfig')
+	use	('onsails/lspkind.nvim')
+	use	("hrsh7th/cmp-nvim-lsp")
+	--	Dependencies
+	use	('kyazdani42/nvim-web-devicons')
+	use	('nvim-lua/plenary.nvim')
 
-	--Floaterm
-	use ('voldikss/vim-floaterm')
-
-	--Smart Column
-	use ("m4xshen/smartcolumn.nvim")
 end)
