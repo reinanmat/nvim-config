eb6d819524bd4b5ab015f7037353f5fc9b800d25
reinanmat <reinan266@gmail.com>
2023-03-17 12:24:22 -0300

    feat: added a remap to deselect

--- lua/myLuaModule/remap.lua
+++ lua/myLuaModule/remap.lua
@@ -11,12 +11,14 @@ keymap.set("n", "<leader>o", vim.cmd.bd)
 keymap.set("n", "<leader>h", ":Stdheader<CR>")
 keymap.set("n", "<leader>g", "gg=G")
 keymap.set("n", "<leader>w", ":w<CR>")
-keymap.set("n", "<leader>q", ":q<CR>")
+keymap.set("n", "<leader>q", ":wq<CR>")
 
 keymap.set("n", "<C-j>", "<cmd>m +1 <CR>")
 keymap.set("n", "<C-k>", "<cmd>m.-2 <CR>")
 keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
 keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
+
+keymap.set("n", "<leader>n", "<cmd>nohlsearch<CR>")
 
 keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
