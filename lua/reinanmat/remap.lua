2cc1ab437cd3f801b55a231860ff0817409ec781
reinanmat <reinan266@gmail.com>
2023-03-20 00:00:04 -0300

    feat: add remap for splits

--- lua/reinanmat/remap.lua
+++ lua/reinanmat/remap.lua
@@ -13,6 +13,13 @@ keymap.set("n", "<leader>g", "gg=G")
 keymap.set("n", "<leader>w", ":w<CR>")
 keymap.set("n", "<leader>q", ":wq<CR>")
 
+keymap.set("n", "x", '"_x')
+
+keymap.set("n", "<leader>sv", "<C-w>v")
+keymap.set("n", "<leader>sh", "<C-w>s")
+keymap.set("n", "<leader>se", "<C-w>=")
+keymap.set("n", "<leader>sx", ":close<CR>")
+
 keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
 keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
 
