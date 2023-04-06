4d757af99e4ea4cafcff1b5b6eb423e64ba24485
reinanmat <92590079+reinanmat@users.noreply.github.com>
2023-04-06 19:39:38 -0300

    refactor: removing harpoon

--- lua/reinanmat/packer.lua
+++ lua/reinanmat/packer.lua
@@ -53,40 +53,47 @@ return require('packer').startup(function(use)
 	use	('williamboman/mason-lspconfig.nvim')
 	use	('williamboman/mason.nvim')
 	use	('wbthomason/packer.nvim')
+
 	--	Navigation
 	use	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.0',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
-	use	('ThePrimeagen/harpoon')
 	use	('prichrd/netrw.nvim')
 	use	('mbbill/undotree')
 	use ('christoomey/vim-tmux-navigator')
+
 	--	Syntax Highlighting
 	use	('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 	use	('Vimjas/vim-python-pep8-indent')
 	use	('nvim-treesitter/playground')
 	use	('tpope/vim-commentary')
+
 	--	Aesthetics
 	use	({ 'rose-pine/neovim', as = 'rose-pine' })
 	use ('nvim-lualine/lualine.nvim')
 	use ('yorik1984/lualine-theme.nvim')
 	use	('m4xshen/smartcolumn.nvim')
+
 	--	42 Plugins
 	use	("42paris/42header")
+
 	--	Autocompletion
 	use	('hrsh7th/cmp-buffer')
 	use	('hrsh7th/nvim-cmp')
 	use	('hrsh7th/cmp-path')
+
 	--	Snippets
 	use	('rafamadriz/friendly-snippets')
 	use	('saadparwaiz1/cmp_luasnip')
 	use	('L3MON4D3/LuaSnip')
+
 	--	Lsp
 	use	({ "glepnir/lspsaga.nvim", branch = "main" })
 	use	('neovim/nvim-lspconfig')
 	use	('onsails/lspkind.nvim')
 	use	("hrsh7th/cmp-nvim-lsp")
+
 	--	Dependencies
 	use	('kyazdani42/nvim-web-devicons')
 	use	('nvim-lua/plenary.nvim')
